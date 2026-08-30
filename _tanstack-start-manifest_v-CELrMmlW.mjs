29f9d07f8db14f78255847824e8cd030d4a403e0
mahirshaikh9906-code <mahirshaikh9906@gmail.com>
2026-08-30 02:47:19 -0700

    Add files via upload

---
+++ _tanstack-start-manifest_v-CELrMmlW.mjs
@@ -0,0 +1,20 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-CELrMmlW.js
+var tsrStartManifest = () => ({ routes: {
+	__root__: {
+		filePath: "/workspace/src/routes/__root.tsx",
+		children: ["/"],
+		preloads: ["/assets/index-lFH4Wr9N.js"],
+		scripts: [{ attrs: {
+			type: "module",
+			async: !0,
+			src: "/assets/index-lFH4Wr9N.js"
+		} }]
+	},
+	"/": {
+		filePath: "/workspace/src/routes/index.tsx",
+		children: void 0,
+		preloads: ["/assets/routes-DieWxBa5.js"]
+	}
+} });
+//#endregion
+export { tsrStartManifest };
